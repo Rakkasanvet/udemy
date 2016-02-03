192cf535887cc0299c97feeec241740a5f7a9f4a
Jason Allen Gill <black6romeo@yahoo.com>
2016-02-03 13:39:32 -0600

    gp2 setup

---
+++ Lecture_67_exercise_3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture_67_exercise_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
